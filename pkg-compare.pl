e8f27d6aa227f0e1b316ea8f69105134bfd70e4a
Jos Boumans <kane@cpan.org>
2008-12-09 16:04:08 +0000

    r5951@rimmer:  kane | 2008-12-09 17:05:31 +0100  * update for RT tickets
    git-svn-id: http://oss.dwim.org/dev-bin@2468 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- pkg-compare.pl
+++ pkg-compare.pl
@@ -1,19 +1,26 @@
 use strict;
 use warnings;
 
+
 use FindBin;
 use File::Fetch;
 use Data::Dumper;
 use CPANPLUS::Backend;
+use CPANPLUS::Shell             qw[Default];
+use LWP::Simple                 qw[get];
 
 $File::Fetch::DEBUG     = 1;
 $Data::Dumper::Indent   = 1;
+
+### RT uri
+my $ListUri = 'http://rt.cpan.org/NoAuth/bugs.tsv?Dist=';
 
 ### include dev-dirs in @INC;
 my @Inc = grep { -d $_ } 
           map  { chomp; "$FindBin::Bin/../$_/lib" } `ls -1 $FindBin::Bin/..`;
 my $U   = 'CPANPLUS::Internals::Utils';
-my $cb  = CPANPLUS::Backend->new;
+my $sh  = CPANPLUS::Shell->new;
+my $cb  = $sh->backend;
 
 my %seen;
 my %version;
@@ -33,42 +40,57 @@ my @mods =  map {
             $cb->author_tree('KANE')->distributions;
 
 ### check our local stuff only
-{   local @INC = @Inc;
-    for my $obj ( sort { $a->module cmp $b->module } values %seen ) {
-        
-        ### version out of date
-        if( $U->_vcmp( $obj->installed_version, $obj->version ) ) {
-            
-            print   "\n\n================================\n\n";
-            printf  "%-42s %-18s %-18s\n",
-                    $obj->module,
+{   for my $obj ( sort { $a->module cmp $b->module } values %seen ) {
+
+        ### header
+        printf "\n\n================ %s ===============\n\n", $obj->module;
+
+
+        ### version info
+        {   local @INC = @Inc;
+            printf  "%-12s %-18s %-18s\n\n",
+                    'VERSIONS',
                     "[HAVE: " . $obj->installed_version   .'] ',
                     "[CPAN: " . $obj->version             .'] ';   
+        }
+        
+        ### outstanding issues in RT
+        {   ### don't care about the diagnostics from this module
+            no warnings 'redefine';
+            local *CPANPLUS::Shell::Default::Plugins::RT::msg   = sub {};
+            local *CPANPLUS::Shell::Default::Plugins::RT::msg   = sub {};
+            local *CPANPLUS::Shell::Default::Plugins::RT::error = sub {};
+            local *CPANPLUS::Shell::Default::Plugins::RT::error = sub {};
 
+            print   "Outstanding issues in RT:\n";
+            $sh->dispatch_on_input( input => '/rt ' . $obj->module );        
+        }
+        
         ### same version, BUT there may be patches applied, check svk log
-        } else {
+        {   local @INC = @Inc;
+            unless( $U->_vcmp( $obj->installed_version, $obj->version ) ) {
 
-            my $log_dir = $obj->installed_dir . '/..';
-            my @out = grep { length } split /---+/, join '', `svk log $log_dir`;
-
-            my $ver = $obj->version;
-
-            my $msg;
-            my $i;
-            for( @out ) {
-                last if /this\s+(?:be|is)\s+$ver/i;         # release
-                last if /\ncopy\n/;                         # copy only
-                next if /^\s*?r\d+:\s*?kane[^\n]+\n\s*$/;   # empty commit
-                $i++;
-                $msg .= $_;
+                my $log_dir = $obj->installed_dir . '/..';
+                my @out = grep { length } split /---+/, join '', `svk log $log_dir`;
+    
+                my $ver = $obj->version;
+    
+                my $msg;
+                my $i;
+                for( @out ) {
+                    last if /this\s+(?:be|is)\s+$ver/i;         # release
+                    last if /\ncopy\n/;                         # copy only
+                    next if /^\s*?r\d+:\s*?kane[^\n]+\n\s*$/;   # empty commit
+                    $i++;
+                    $msg .= $_;
+                }
+                
+                ### nothing to see, move along
+                next unless $i;
+    
+                printf  "%-42s (%s outstanding patches)\n%s", $obj->module, $i, $msg;
+                
             }
-            
-            ### nothing to see, move along
-            next unless $i;
-
-            print   "\n\n================================\n\n";
-            printf  "%-42s (%s outstanding patches)\n%s", $obj->module, $i, $msg;
-            
         }
     }
 }
