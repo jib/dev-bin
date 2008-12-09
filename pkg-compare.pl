011f63534eb7c8471603a7c79834662f17aca71d
Jos Boumans <kane@cpan.org>
2008-12-09 16:04:05 +0000

    r5950@rimmer:  kane | 2008-12-09 15:13:51 +0100  * now checks for svk commits as well if versions are equal
    git-svn-id: http://oss.dwim.org/dev-bin@2467 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- pkg-compare.pl
+++ pkg-compare.pl
@@ -6,9 +6,8 @@ use File::Fetch;
 use Data::Dumper;
 use CPANPLUS::Backend;
 
-
-$File::Fetch::DEBUG =1 ;
-$Data::Dumper::Indent = 1;
+$File::Fetch::DEBUG     = 1;
+$Data::Dumper::Indent   = 1;
 
 ### include dev-dirs in @INC;
 my @Inc = grep { -d $_ } 
@@ -36,14 +35,40 @@ my @mods =  map {
 ### check our local stuff only
 {   local @INC = @Inc;
     for my $obj ( sort { $a->module cmp $b->module } values %seen ) {
-        next unless $U->_vcmp( $obj->installed_version, $obj->version ) == 1;
-        printf  "%-42s %-18s %-18s\n",
-                $obj->module,
-                "[HAVE: " . $obj->installed_version   .'] ',
-                "[CPAN: " . $obj->version             .'] ';   
+        
+        ### version out of date
+        if( $U->_vcmp( $obj->installed_version, $obj->version ) ) {
+            
+            print   "\n\n================================\n\n";
+            printf  "%-42s %-18s %-18s\n",
+                    $obj->module,
+                    "[HAVE: " . $obj->installed_version   .'] ',
+                    "[CPAN: " . $obj->version             .'] ';   
+
+        ### same version, BUT there may be patches applied, check svk log
+        } else {
+
+            my $log_dir = $obj->installed_dir . '/..';
+            my @out = grep { length } split /---+/, join '', `svk log $log_dir`;
+
+            my $ver = $obj->version;
+
+            my $msg;
+            my $i;
+            for( @out ) {
+                last if /this\s+(?:be|is)\s+$ver/i;         # release
+                last if /\ncopy\n/;                         # copy only
+                next if /^\s*?r\d+:\s*?kane[^\n]+\n\s*$/;   # empty commit
+                $i++;
+                $msg .= $_;
+            }
+            
+            ### nothing to see, move along
+            next unless $i;
+
+            print   "\n\n================================\n\n";
+            printf  "%-42s (%s outstanding patches)\n%s", $obj->module, $i, $msg;
+            
+        }
     }
 }
-
-#print join $/, map { $_->module } @mods;
-
-#print Dumper \%seen;
